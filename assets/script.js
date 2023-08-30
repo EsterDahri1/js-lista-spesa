645cc083eae54c83f2fb7acb43fd7a5fb5c4fdd0
EsterDahri <esterdahri@gmail.com>
2023-08-30 14:00:11 +0200

    add array in js and ul in html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,38 @@
-console.log('hello!');
+//console.log('hello!');
+
+/*Consegna:
+- Data una lista della spesa
+- stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente 
+- con un ciclo while.
+
+##Tools
+- array
+- print on page individually (ul)
+- use while
+*/
+
+//Create and array that contains a grocery's list
+const groceryList = [
+    'Apples',
+    'Bananas',
+    'Strawberries',
+    'Avocados',
+    'Bell Peppers',
+    'Carrots',
+    'Broccoli',
+    'Garlic',
+    'Lemons/Limes',
+    'Onion',
+    'Parsley',
+    'Cilantro',
+    'Basil',
+    'Potatoes',
+    'Spinach',
+    'Tomatoes'
+];
+console.log(groceryList);
+
+//use while to create loop
+//Print on page by using query selector of ul
+
+
